78b4284d60fec536e56cdd525a6dd30043cda54e
stephenfortune <stephen.j.fortune@gmail.com>
2015-04-20 17:29:36 +0100

    grid function renders a set of coloured bg circles

--- calculations.js
+++ calculations.js
@@ -11,3 +11,15 @@ console.log(((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilli
 console.log("current time in seconds: ");
 console.log(time.getMilliseconds());
 console.log(Math.PI*1.99999999999999999999999999999999999999999999999999999999);
+console.log((Math.PI*1.99)/60/24);
+
+var gridCalc = function(dim, grid){
+    // assumes that a square space is to be plotted
+    var gridRows = Math.sqrt(grid);
+    centre = (dim/gridRows)/2;
+    radius = (dim/gridRows)/gridRows;
+    console.log("centre = "+centre+", radius = "+radius);
+
+};
+
+gridCalc(650, 9);
