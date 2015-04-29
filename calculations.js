7e8531ae5279acea1449af2ea0d492af5669014b
stephenfortune <stephen.j.fortune@gmail.com>
2015-04-29 17:20:05 +0100

    move, refactor, additional roughwork in calculations

--- calculations.js
+++ calculations.js
@@ -10,7 +10,9 @@ console.log(((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilli
 // between
 console.log("current time in seconds: ");
 console.log(time.getMilliseconds());
-console.log(Math.PI*1.99999999999999999999999999999999999999999999999999999999);
+console.log("max radian threshold of arc")
+console.log(Math.PI*1.99);
+console.log("above value divided by 60 [secs] then divided by 24 [frame per second]")
 console.log((Math.PI*1.99)/60/24);
 
 var gridCalc = function(dim, grid){
@@ -23,4 +25,28 @@ var gridCalc = function(dim, grid){
 };
 
 gridCalc(650, 9);
-console.log((Math.random() * 24) + 1);
+
+var planetData = new Array(58.81337, 243.68663, 1, 1.028552, 0.41435, 0.44499, 0.72006, 0.67339, 6.40529);
+
+console.log("pre sort");
+console.log(planetData);
+function sortFloat(a,b) { return a - b; }
+planetData.sort(sortFloat);
+console.log("post sort");
+console.log(planetData);
+console.log("lowest = "+planetData[0]+", and highest = "+planetData[planetData.length-1]);
+var span = planetData[planetData.length-1] - planetData[0];
+console.log("span = "+span);
+console.log("span divided by 0.00 and 1.99 = "+span);
+
+for (i=0; i < (span/1.99); i++){
+    //console.log(i);
+}
+
+/*
+* arr[0] = 0.104666667 || Math.Pi * 1.99 / 60
+* span = arr[arr.length-1] - arr[0]
+* span segmented between 0.00 and 1.99
+* HOW TO GET THE RIGHT STEP
+* span / 1.99
+* */
